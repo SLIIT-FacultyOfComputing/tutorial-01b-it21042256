699497b88ab4a7a45e15d9bbf83bf0a5eb5bc918
IT21042256 <87684692+it21042256@users.noreply.github.com>
2021-07-26 10:02:14 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  int mark1, mark2;
+float avg;
+
+printf("Enter mark for subject 1:");
+scanf("%d", &mark1);
+
+printf("Enter mark for subject 2:");
+scanf("%d", &mark2);
+
+avg =(mark1+mark2)/2;
+
+printf("Average of %d and %d is: %.2f", mark1,mark2,avg);
   
   return 0;
 }
